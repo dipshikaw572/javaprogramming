f11bb89a0471e85953273e6f9669970d025515a3
dipshikaw572 <127364189+dipshikaw572@users.noreply.github.com>
2023-03-09 08:28:37 +0530

    Create Data_type.java

---
+++ Data_type.java
@@ -0,0 +1,14 @@
+class Datatype{
+
+public static void main(String[] args) {
+byte b=32;
+short s=1508;
+int i=956182604;
+long l=967183104;
+System.out.println(b);
+System.out.println(s);
+System.out.println(i);
+System.out.println(l);
+}
+
+}
